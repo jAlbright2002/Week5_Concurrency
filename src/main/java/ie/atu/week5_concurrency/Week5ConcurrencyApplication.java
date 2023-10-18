baf8220554453ada38ed7725bdc6ac134bab6204
JamesA <G00379271@atu.ie>
2023-10-18 14:26:54 +0100

    Create project

---
+++ src/main/java/ie/atu/week5_concurrency/Week5ConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5ConcurrencyApplication.class, args);
+    }
+
+}
